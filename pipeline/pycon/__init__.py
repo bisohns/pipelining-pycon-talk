073a20a28345806cbff2551277add7181089de7b
Manasseh Mmadu <mmadumanasseh@gmail.com>
2019-10-28 22:49:13 +0100

    Started Django App and added pycon asciimatics

---
+++ pipeline/pycon/__init__.py
@@ -0,0 +1,27 @@
+from asciimatics.screen import Screen
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+
+
+def display(screen):
+    effects = [
+        Cycle(
+            screen,
+            FigletText("WELCOME TO", font='big'),
+            int(screen.height / 2 - 8)),
+        Cycle(
+            screen,
+            FigletText("PYCON!", font='big'),
+            int(screen.height / 2 + 3)),
+        Stars(screen, 200)
+    ]
+    screen.play([Scene(effects, 500)])
+
+
+
+def demo():
+    Screen.wrapper(display)
+
+if __name__ == '__main__':
+    Screen.wrapper(display)
