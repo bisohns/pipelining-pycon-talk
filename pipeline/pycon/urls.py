6c2082d46ce1c4e53b1a5b50938e478cab9177f2
Manasseh Mmadu <mmadumanasseh@gmail.com>
2019-10-28 23:49:22 +0100

    Added Pycon URL

---
+++ pipeline/pycon/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from pycon import views
+
+app_name = "pycon"
+urlpatterns = [
+    path("pycon/", views.PyconView.as_view(), name="pycon"),
+]
--- pipeline/pycon/views.py
+++ pipeline/pycon/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class PyconView(TemplateView):
+    template_name = "pycon.html"
