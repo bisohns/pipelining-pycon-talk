3fe787a9995cdfe7d06163e623f01fac37add54e
Manasseh Mmadu <mmadumanasseh@gmail.com>
2019-10-29 01:35:34 +0100

    Updated Version

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pycon import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pycon.urls')),
+    path("", views.PyconView.as_view(), name="index"),
 ]
